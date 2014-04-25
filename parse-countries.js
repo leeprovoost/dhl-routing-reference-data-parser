7fa3c52ddc48250929b26702b691eeae8014758c
Lee Provoost <lee.provoost@gmail.com>
2014-04-26 00:07:31 +0100

    convert N/Y string values to false/true boolean values

--- parse-countries.js
+++ parse-countries.js
@@ -2,33 +2,45 @@
 // See developer documentation here: http://www.dhl.co.uk/content/gb/en/express/resource_centre/integrated_shipping_solutions/developer_download_centre1.html
 
 var Transform = require('stream').Transform
-  , csv = require('csv-streamify')
-  , JSONStream = require('JSONStream')
-  , _ = require('lodash');
+  	, csv = require('csv-streamify')
+  	, JSONStream = require('JSONStream')
+  	, fs = require('fs')
+  	, _ = require('lodash');
+
+var fstream = fs.createReadStream('source/country.txt');
 
 // see parser options defined here: https://github.com/klaemo/csv-stream
 var csvToJson = csv({objectMode: true, delimiter: '|' });
 
 var parser = new Transform({objectMode: true});
 parser._transform = function(data, encoding, done) {
-  this.push(this._parseRow(data));
-  done();
+  	this.push(this._parseRow(data));
+  	done();
 };
 
 // Parse a data row into an object
 parser._parseRow = function(row) {
+	// convert Postcode Flag N/Y to booleans
+	var postcode_flag = false;
+	if (row[10] == "Y") {
+		postcode_flag = true;
+	}
+	// TODO filter out some dummy entries
+	//if (row[0]=="XX" || row[0]=="XL" || row[0]=="XA") {
+	//}
+
 	// filter the columns we are interested in, we're using single letter column names to save
 	// space in our MongoDB database
 	// a: Country Code, b: Country Name, c: Currency Code, d: Use Postcode Flag
 	var result = _.zipObject(
 		['a','b','c','d'], 
-		[row[0],row[1],row[8],row[10]]
+		[row[0],row[1],row[8],postcode_flag]
 	);
   	
   	return result;
 };
 
-process.stdin
+fstream
 .pipe(csvToJson)
 .pipe(parser)
 .pipe(JSONStream.stringify(false))
