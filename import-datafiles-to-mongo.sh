c7812b24acdb15519846b496eb5252e653328590
Lee Provoost <lee.provoost@gmail.com>
2014-06-14 22:16:32 +0100

    reduced file size of generated json documents

--- import-datafiles-to-mongo.sh
+++ import-datafiles-to-mongo.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 echo Step 1 of 4: Split large JSON document in smaller chunks so that mongoimport can handle them
-split -l 300000 output/ESDv6.json
+split -l 250000 output/ESDv6.json "ESDv6_small_"
 mv xa* output
 
 echo Step 2 of 4: Uploading ESD files to MongoDB...
-mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/xaa --jsonArray
-mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/xab --jsonArray
-mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/xac --jsonArray
-mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/xad --jsonArray
-mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/xae --jsonArray
-mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/xaf --jsonArray
-mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/xag --jsonArray
-mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/xah --jsonArray
-mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/xai --jsonArray
-mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/xaj --jsonArray
-mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/xak --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_aa --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_ab --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_ac --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_ad --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_ae --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_af --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_ag --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_ah --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_ai --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_aj --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_ak --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_al --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_am --jsonArray
+mongoimport --db test --collection intl_postcode_api_ESD --fields a,b,c,d --file output/ESDv6_small_an --jsonArray
 
 echo Step 3 of 4: Uploading country file to MongoDB...
 mongoimport --db test --collection intl_postcode_api_country --fields a,b,c,d --file output/countries.json --jsonArray
